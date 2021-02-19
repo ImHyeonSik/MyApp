7b967ce862237ceb123d08391308cf8ba39fb376
hyeonsik <hsim@onesoftdigm.com>
2021-02-19 15:44:23 +0900

    네비게이션(탭+스택)연결, tab Bar 이미지 변경

---
+++ view/TabApp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Image } from "react-native";
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from "react-navigation-stack";
+import HomeScreen from "./HomeScreen";
+import SettingScreen from "./SettingScreen";
+import Food from "./food";
+
+
+const HomeTabNavigator = createStackNavigator(
+  {
+    TabStackHome : HomeScreen,
+    FoodPage: Food
+  }
+);
+
+const SettingTabNavigator = createStackNavigator(
+  {
+    TabStackSetting : SettingScreen,
+  }
+);
+
+const Tab = createBottomTabNavigator (
+  {
+    TabHome : HomeTabNavigator,
+    SettingPage: SettingTabNavigator,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+        tabBarLabel: ' ',
+        tabBarIcon: ({ focused, horizontal,}) => {
+          const { routeName } = navigation.state;
+          let iconName;
+          if (routeName === 'TabHome') {
+            iconName = focused
+              ? require('../src/common/img/navigation/home_on.png')
+              : require('../src/common/img/navigation/home_off.png')
+          } else if (routeName === 'SettingPage') {
+            iconName = focused ? require('../src/common/img/navigation/setting_on.png') : require('../src/common/img/navigation/setting_off.png');
+          }
+          return <Image source={iconName} style={{width:30, height: 30, marginTop: 40}}/>;
+        }
+    }
+    )}
+);
+export default Tab;
